c74717626cb23e9649dd14f7730ad9776606be15
LoxiasMegalos <murillo.alkantara@gmail.com>
2022-01-04 20:12:52 -0300

    Aula 2 Finalizada: input | conversão de input | if | identação

--- aula_2_alura.py
+++ aula_2_alura.py
@@ -1,5 +1,22 @@
 #Alura_aula_2_curso_1_Python
-print("Este programa é a aula 2 do curso 1 de Python da ALURA")
-print()
-print("teste commit 1", end="\n")
-print("teste commit 2")
+#print("Este programa é a aula 2 do curso 1 de Python da ALURA", end="\n")
+print("*******************************************", end="\n")
+print("Bem vindo ao jogo de adivinhação!", end="\n")
+print("*******************************************")
+
+numero_secreto = 42
+
+# chute = int(input("Digite o seu numero: "))
+# ou
+chute_str = input("Digite o seu numero: ")
+
+chute = int(chute_str)
+
+print("Você digitou ", chute)
+
+if(chute == numero_secreto):
+    print("Você acertou o número secreto")                           #identação de 4 espaços para reconhecer o bloco
+else:
+    print("O número secreto era:", numero_secreto,", mas você digitou:",chute)
+
+print("Fim do jogo")
