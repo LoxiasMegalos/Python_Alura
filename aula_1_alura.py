fc69704258bbd2f4911b3ca8a09a3f875a3ee15d
LoxiasMegalos <murillo.alkantara@gmail.com>
2022-01-04 15:51:08 -0300

    .

---
+++ aula_1_alura.py
@@ -0,0 +1,21 @@
+#pais = "Brasil"
+#quantidade = 5
+
+#Função Print e Type
+
+#print(pais, "é", type(pais), end="\n")
+#print(quantidade, "é", type(quantidade), end="\n")
+#print(pais, "ganhou", quantidade, "titulos", "mundiais", sep="-",end="\n")
+
+#dia = 15
+#mes = 10
+#ano = 2015
+#print(dia, mes, ano, sep="/", end="\n")
+
+#Tipagem Dinânima
+pais = "Brasil"
+print(type(pais), end="\n")
+pais = 644
+print(type(pais), end="\n")
+pais = 1.1
+print(type(pais), end="\n")
