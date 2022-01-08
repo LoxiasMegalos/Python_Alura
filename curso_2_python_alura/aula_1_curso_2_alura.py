5e3c974358d4b1cb97c183ba870f94338ec842de
LoxiasMegalos <murillo.alkantara@gmail.com>
2022-01-08 12:36:08 -0300

    Aula 08/01/2021 - Inicio do curso 2 de Python

--- curso_2_python_alura/aula_1_curso_2_alura.py
+++ curso_2_python_alura/aula_1_curso_2_alura.py
@@ -1,1 +1,32 @@
+import re
+
 print("Este é o primeiro programa do segundo curso de Python da Alura - avançando na linguagem")
+
+def forca():
+    print("**********************************************")
+    print("**********BEM VINDO AO JOGO DA FORCA**********")
+    print("**********************************************")
+
+    palavra_secreta = "banana"
+    enforcou = False
+    acertou = False
+
+    #enquanto (True)
+    while(not enforcou and not acertou):
+        chute = input("Digite uma letra: ")
+        if(re.search(chute, palavra_secreta)):
+            print("Voce acertou")
+            enforcou = True
+            acertou = True
+        else:
+            print("Voce errou")
+
+
+        #print("Jogando")
+
+
+    print("Fim de Jogo! Obrigado por jogar!")
+
+
+if(__name__ == "__main__"):
+    forca()
