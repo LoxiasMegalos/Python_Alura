45e7fb249ca690283cce925b09091f71c1877656
LoxiasMegalos <murillo.alkantara@gmail.com>
2022-01-13 20:41:01 -0300

    Aulas 13/01/2021 - Inicio do Curso 3 Python Alura

--- curso_3_python_alura/aula_1_curso_3_alura.py
+++ curso_3_python_alura/aula_1_curso_3_alura.py
@@ -1,1 +1,28 @@
 #PRIMEIRA AULA DO CURSO 3 DA FORMAÇÃO DE PYTHON ALURA
+#Orientação a objetos
+#CONTA BANCÁRIA
+
+def cria_conta(numero, titular, saldo, limite):
+    conta = {"numero": numero, "titular": titular, "saldo": saldo, "limite": limite}
+    return conta
+
+def deposita(conta, valor):
+    conta["saldo"] += valor
+
+def saca(conta, valor):
+    conta["saldo"] -= valor
+
+def extrato(conta):
+    print("Saldo é {}".format(conta["saldo"]))
+
+
+#COMO FAZER ISSO PARA MAIS CONTAS?
+
+#precisamos de ujma função
+
+conta = cria_conta(123, "Murillo", 55.0, 1000.00)
+conta["saldo"]
+deposita(conta, 15.0)
+extrato(conta)
+saca(conta, 20.0)
+extrato(conta)
