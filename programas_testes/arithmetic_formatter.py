3144355e91e91bd15b52a37e4694f19a63b71db0
LoxiasMegalos <murillo.alkantara@gmail.com>
2022-02-04 20:36:27 -0300

    04/02/2021 - Projeto 1 - Freecodecamp - Arithmetic Formatter

---
+++ programas_testes/arithmetic_formatter.py
@@ -0,0 +1,53 @@
+string = ["555 + 555", "1 - 1", "9999 + 9998", "0 + 0", "9999 - 9999"]
+lista_cima = []
+lista_meio = []
+lista_sep = []
+lista_resultado = []
+for termo in range(len(string)):
+    y = ""
+    for elemento in string[termo]:
+
+        if elemento == "-" or elemento == "+" :
+            y = y.strip()
+            a = int(y)
+            operacao = elemento
+            y = ""
+
+        y += elemento
+
+    if y.startswith("+"):
+        y = y[1:]
+    elif y.startswith("-"):
+        y = y[1:]
+
+    y = y.strip()
+    b = int(y)
+    if operacao == "+":
+        resultado = a + b
+    elif operacao == "-":
+        resultado = a - b
+    if a > b:
+        espacos = len(str(a)) + 2
+    else:
+        espacos = len(str(b)+operacao) + 1
+
+    n = espacos - len(str(a))
+    cima = n*" "+ str(a)
+    n = espacos - len(str(b)+operacao)
+    meio = operacao + n*" "+str(b)
+    baixo = espacos*"-"
+    n = espacos - len(str(resultado))
+    s_resultado = n*" "+ str(resultado)
+    lista_cima.append(cima)
+    lista_meio.append(meio)
+    lista_sep.append(baixo)
+    lista_resultado.append(s_resultado)
+
+print(*lista_cima, sep="    ")
+print(*lista_meio, sep="    ")
+print(*lista_sep, sep="    ")
+print(*lista_resultado, sep="    ")
+
+
+
+
